bc29f5e23332fd5dff1d74aa518a2e6cf938e083
Henrique Assis <henrique_assis_10@hotmail.com>
2020-09-28 11:46:24 -0300

    up

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import GlobalStyles from './styles/global';
+import { Main } from './styles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+
+      <Main style={{height: '1616vh'}}> 
+
+      </Main>
+    </>
+  );
+}
+
+export default App;
